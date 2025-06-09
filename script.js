fbbf1e76c71ec1518073437ca2ae6d66694da147
stillbencabal <cabalstillben@gmail.com>
2025-06-09 12:21:21 -0700

    Clean Initial

--- script.js
+++
@@ -1,5 +0,0 @@
-// Example: Display an alert when the website loads
-window.onload = function() {
-    alert('Welcome to my static website!');
-};
-
--- test.js
+++
@@ -1,7 +0,0 @@
-function getFullName(first, last)
-{
-    let fullName = first + " " + last;
-    return fullName;
-}
-
-console.log(getFullName("Still Ben", "Cabal"));
